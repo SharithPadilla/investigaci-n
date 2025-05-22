883ab8511f8b87a623738f2011ba1dc8b2efdac4
SharithPadilla <padillasharith28@.gmail.com>
2025-05-22 14:57:30 -0700

    adjunto primer archivo

---
+++ multiplicación.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+void multi (){
+    int num1;
+     int num2;
+
+    cout << "primer numero a multiplicar" << endl; 
+    cin>> num1;
+    cout << "segundo numero a multiplicar" << endl; 
+    cin >> num2;
+    
+    cout << "el resultado de la multiplicacion es:" << num1 * num2 << endl; 
+
+   
+}
+
+int main (){
+   
+    multi();
+     return 0;
+}
